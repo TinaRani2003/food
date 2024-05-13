0f744ed1aa487aed5c031449c7929cc0b5d418d7
root <root@ip-172-31-31-25.ec2.internal>
2024-05-13 10:54:36 +0000

    Footer changed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,13 +7,13 @@ const Footer = () => {
         <div className="banner">
           <div className="left">SpiceSavor</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
+            <p>Z6 Gulshan-e-Maymar, India</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By TINA RANI MAHATO</p>
           </div>
           <div className="right">
             <p>All Rights Reserved By CodeWithZeeshu.</p>
